82015ee3f8a192ebba2bc7be2842e8f5ec18d9b0
Ray-Arthur <27481892+StarZHF@users.noreply.github.com>
2021-01-09 02:07:44 -0600

    Update frmMain.cs

--- EncoderExample/frmMain.cs
+++ EncoderExample/frmMain.cs
@@ -34,7 +34,7 @@ namespace EncoderExample
         private void btnEncode_Click(object sender, EventArgs e)
         {
             byte[] split = Encoding.Default.GetBytes("#defaultSplit#"); // our Default split.
-            byte[] Encoded = Encoding.Default.GetBytes(Convert.ToBase64String(File.ReadAllBytes(tbPath.Text))); // out Base64 encoded payload.
+            byte[] Encoded = Encoding.Default.GetBytes(Convert.ToBase64String(File.ReadAllBytes(tbPath.Text))); // our Base64 encoded payload.
             byte[] stub = File.ReadAllBytes(Environment.CurrentDirectory + @"\stub.exe"); // the stub
 
             IEnumerable<byte> output = stub.Concat(split).Concat(Encoded); // concatenating bytes through LINQ.
